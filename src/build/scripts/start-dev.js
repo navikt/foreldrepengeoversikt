1c521624b432daabe7e7314221e157bcede3fb14
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-10-01 11:34:56 +0200

    Change dev port to 8880

--- src/build/scripts/start-dev.js
+++ src/build/scripts/start-dev.js
@@ -13,6 +13,7 @@ createEnvSettingsFile(path.resolve(`${__dirname}/../../../dist/js/settings.js`))
 getDecorator().then((decoratorData) => {
     const compiler = webpack(webpackConfig);
     const server = new WebpackDevServer(compiler, configureDevServer(decoratorData));
+    const port = 8880;
 
-    server.listen(8080, '127.0.0.1', () => console.log('Started server on http://localhost:8080'));
+    server.listen(port, '127.0.0.1', () => console.log(`Started server on http://localhost:${port}`));
 });
