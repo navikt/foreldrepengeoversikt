0c0fa07c862dd2a9e6cb1e79366b732fb427929d
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2019-01-14 09:41:20 +0100

    Vis empty-state-melding i et veilederpanel

--- src/app/components/ingen-saker/IngenSaker.tsx
+++ src/app/components/ingen-saker/IngenSaker.tsx
@@ -1,28 +1,31 @@
 import * as React from 'react';
 import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
-import Lenke from 'nav-frontend-lenker';
-import KnappBase from 'nav-frontend-knapper';
-
-import CryBabyIcon from 'app/components/ikoner/CryBabyIkon';
-import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import { lenker } from '../../utils/lenker';
+import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import BEMHelper from 'common/util/bem';
-
+import classnames from 'classnames';
+import CryBabyIcon from 'app/components/ikoner/CryBabyIkon';
+import KnappBase from 'nav-frontend-knapper';
+import Lenke from 'nav-frontend-lenker';
+import Veileder from 'common/components/veileder/Veileder';
+import Veilederpanel from 'nav-frontend-veilederpanel';
 import './ingenSaker.less';
 
 const IngenSaker = () => {
     const cls = BEMHelper('ingen-saker');
     return (
-        <div className={cls.className}>
+        <div className={classnames(cls.className, 'blokk-m')}>
             <CryBabyIcon className={cls.element('icon')} />
 
             <Undertittel className={cls.element('title')}>
                 <FormattedMessage id={'ingenSaker.title'} />
             </Undertittel>
 
-            <Normaltekst className={cls.element('ingress')}>
-                <FormattedHTMLMessage id={'ingenSaker.ingress'} />
-            </Normaltekst>
+            <Veilederpanel kompakt={true} svg={<Veileder />}>
+                <Normaltekst className={cls.element('ingress')}>
+                    <FormattedHTMLMessage id={'ingenSaker.ingress'} />
+                </Normaltekst>
+            </Veilederpanel>
 
             <div className={cls.element('options')}>
                 <Lenke href={lenker.lesMerOmForeldrepenger}>
