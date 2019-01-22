ec4e703af246d85cbb2de91c223c0b1f639e63ef
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-01-22 09:39:28 +0100

    Type BehandlingResultatType

--- src/app/types/Behandling.ts
+++ src/app/types/Behandling.ts
@@ -8,10 +8,31 @@ export default interface Behandling {
     tema: string;
     type: string;
     årsak: BehandlingÅrsak;
-    behandlingResultatType: string;
+    behandlingResultatType: BehandlingResultatType;
     inntektsmeldinger: string[];
 }
 
+export enum BehandlingResultatType {
+    IKKE_FASTSATT = "IKKE_FASTSATT",
+    INNVILGET = "INNVILGET",
+    AVSLÅTT = "AVSLÅTT",
+    OPPHØR = "OPPHØR",
+    HENLAGT_SØKNAD_TRUKKET = "HENLAGT_SØKNAD_TRUKKET",
+    HENLAGT_FEILOPPRETTET = "HENLAGT_FEILOPPRETTET",
+    HENLAGT_BRUKER_DØD = "HENLAGT_BRUKER_DØD",
+    MERGET_OG_HENLAGT = "MERGET_OG_HENLAGT",
+    HENLAGT_SØKNAD_MANGLER = "HENLAGT_SØKNAD_MANGLER",
+
+    KLAGE_AVVIST = "KLAGE_AVVIST",
+    KLAGE_MEDHOLD = "KLAGE_MEDHOLD",
+    KLAGE_YTELSESVEDTAK_OPPHEVET = "KLAGE_YTELSESVEDTAK_OPPHEVET",
+    KLAGE_YTELSESVEDTAK_STADFESTET = "KLAGE_YTELSESVEDTAK_STADFESTET",
+    HENLAGT_KLAGE_TRUKKET = "HENLAGT_KLAGE_TRUKKET",
+
+    INNSYN_INNVILGET = "INNSYN_INNVILGET",
+    INNSYN_DELVIS_INNVILGET ="INNSYN_DELVIS_INNVILGET",
+    INNSYN_AVVIST = "INNSYN_AVVIST"
+}
 
 export enum BehandlingStatus {
     OPPRETTET = 'OPPRE',
