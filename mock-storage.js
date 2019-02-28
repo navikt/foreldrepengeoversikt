da57348d2d0d78afe621d90828cea9862e262681
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-02-28 09:41:20 +0100

    Add mock API server

---
+++ mock-storage.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const fs = require('fs');
+
+const getFilePath = function(filnavn) {
+    var directories = ['.', 'mock_data', filnavn];
+    return directories.join(path.sep);
+};
+
+const getPersoninfo = function() {
+    const fileName = getFilePath('personinfo.json');
+    if (!fs.existsSync(fileName)) {
+        return {};
+    } else {
+        try {
+            return JSON.parse(fs.readFileSync(fileName, 'utf8'));
+        } catch (err) {
+            return {};
+        }
+    }
+};
+
+const getSaker = function() {
+    const fileName = getFilePath('saker.json');
+    if (!fs.existsSync(fileName)) {
+        return {};
+    } else {
+        try {
+            return JSON.parse(fs.readFileSync(fileName, 'utf8'));
+        } catch (err) {
+            return {};
+        }
+    }
+};
+
+module.exports = {
+    getPersoninfo,
+    getSaker
+};
