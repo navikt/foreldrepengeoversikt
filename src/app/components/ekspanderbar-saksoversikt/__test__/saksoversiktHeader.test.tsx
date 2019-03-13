5437d80bd210c4bd1fef0a2a255c224c591cd6e1
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-03-13 14:27:56 +0100

    Remove old test

--- src/app/components/ekspanderbar-saksoversikt/__test__/saksoversiktHeader.test.tsx
+++ src/app/components/ekspanderbar-saksoversikt/__test__/saksoversiktHeader.test.tsx
@@ -4,22 +4,11 @@ import EtikettBase from 'nav-frontend-etiketter';
 
 import EkspanderbarSaksoversiktHeader from '../EkspanderbarSaksoversiktHeader';
 import SakerMock from '../../../../../jest/__mocks__/Sak';
-import { getIntlKeyForStatus } from '../util';
-import { FagsakStatus } from '../../../types/FagsakStatus';
 
 describe('EkspanderbarSaksoversiktHeader', () => {
     it('Should render status etikett for fpsak saker when status is defined', () => {
         const wrapper = shallow(<EkspanderbarSaksoversiktHeader sak={SakerMock.fpsakSak} />);
         expect(wrapper.find(EtikettBase).length).toEqual(1);
-    });
-
-    it('getIntlKeyForStatus should return correct string', () => {
-        const underBehandlingIntlKey = 'saksoversikt.heading.underBehandling';
-        const avsluttetIntlKey = 'saksoversikt.heading.avsluttet';
-        expect(getIntlKeyForStatus(FagsakStatus.OPPRETTET)).toEqual(underBehandlingIntlKey);
-        expect(getIntlKeyForStatus(FagsakStatus.UNDER_BEHANDLING)).toEqual(underBehandlingIntlKey);
-        expect(getIntlKeyForStatus(FagsakStatus.LOPENDE)).toEqual(avsluttetIntlKey);
-        expect(getIntlKeyForStatus(FagsakStatus.AVSLUTTET)).toEqual(avsluttetIntlKey);
     });
 
     it('Should render status etikett for fpsak saker when status is defined', () => {
