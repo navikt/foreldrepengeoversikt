7b447ffb88ad1c483bea978a1ec2d841a03c60d8
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-03-13 12:20:46 +0100

    Remove ExternalLinkIcon

--- src/app/components/relatert-informasjon/RelatertInformasjon.tsx
+++ src/app/components/relatert-informasjon/RelatertInformasjon.tsx
@@ -5,7 +5,6 @@ import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 
 import BEMHelper from '../../../common/util/bem';
 import { annenInformasjonLenker } from '../../utils/lenker';
-import ExternalLinkIcon from '../ikoner/ExternalLinkIcon';
 
 import './relatertInformasjon.less';
 
@@ -22,7 +21,6 @@ const RelatertInformasjon = () => {
                         <div key={link.href} className={cls.element('link-container')}>
                             <Lenke className={cls.element('link')} href={link.href}>
                                 <Normaltekst>{link.text}</Normaltekst>
-                                <ExternalLinkIcon className={cls.element('link-icon')} height={15} width={15}/>
                             </Lenke>
                         </div>
                     ))}
