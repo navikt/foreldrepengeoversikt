bcb2042152c5374fb0bc8f41a61ef272794268a0
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-08-12 10:56:06 +0200

    Update package.json

--- src/app/pages/dine-foreldrepenger/__tests__/dineForeldrepenger.test.tsx
+++ src/app/pages/dine-foreldrepenger/__tests__/dineForeldrepenger.test.tsx
@@ -5,11 +5,18 @@ import { historyMock } from '../../../../../jest/__mocks__/History';
 import EkspanderbarSaksoversikt from '../../../components/saksoversikt/saksoversikt-ekspanderbar/EkspanderbarSaksoversikt';
 import SakerMock from '../../../../../jest/__mocks__/Sak';
 import IngenSaker from '../../../components/ingen-saker/IngenSaker';
+import Saksoversikt from 'app/components/saksoversikt/saksoversikt-main/Saksoversikt';
 
 describe('Dine Foreldrepenger page', () => {
     it('Should render ingen saker component if saker is an empty list', () => {
         const wrapper = shallow(<DineForeldrepenger saker={[]} history={historyMock} />);
         expect(wrapper.find(IngenSaker).length).toEqual(1);
+    });
+
+    it('Should render Saksoversikt for newest sak', () => {
+        const mockSaker = [SakerMock.infotrygd, SakerMock.fpsakFP];
+        const wrapper = shallow(<DineForeldrepenger saker={mockSaker} history={historyMock} />);
+        expect(wrapper.find(Saksoversikt).length).toEqual(1);
     });
 
     it('Should render EkspanderbarSaksoversikt for each element in saker except the newest', () => {
