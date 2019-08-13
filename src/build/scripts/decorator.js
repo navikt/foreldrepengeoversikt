6963198e65a2ff3dbc6088b680ee9e2c388203ea
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-08-13 12:34:07 +0200

    Hardcode decorator URL

--- src/build/scripts/decorator.js
+++ src/build/scripts/decorator.js
@@ -5,7 +5,7 @@ const { JSDOM } = jsdom;
 
 const requestDecorator = (callback) =>
     request(
-        `${process.env.APPRES_CMS_URL}/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer=true`,
+        `$http://appres.nav.no/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer=true`,
         callback
     );
 
@@ -21,7 +21,7 @@ const getDecorator = () =>
                     NAV_STYLES: document.getElementById('styles')[prop],
                     NAV_HEADING: document.getElementById('header-withmenu')[prop],
                     NAV_FOOTER: document.getElementById('footer')[prop],
-                    NAV_MEGAMENU_RESOURCES: document.getElementById('megamenu-resources')[prop],
+                    NAV_MEGAMENU_RESOURCES: document.getElementById('megamenu-resources')[prop]
                 };
                 resolve(data);
             } else {
