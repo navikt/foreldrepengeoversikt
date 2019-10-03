2020eb1c085d87ec6d61f185821f21fbcaa8d487
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-10-03 12:57:44 +0200

    Update nav-frontend-alertstriper and peer dependencies

--- src/app/components/dokumentoversikt/Dokumentoversikt.tsx
+++ src/app/components/dokumentoversikt/Dokumentoversikt.tsx
@@ -12,7 +12,7 @@ const Dokumentoversikt = () => {
     const cls = BEMHelper('dokumentoversikt');
     return (
         <div className={cls.className}>
-            <AlertStripe className={cls.element('alertstripe')} type="info" solid={true}>
+            <AlertStripe className={cls.element('alertstripe')} type="info">
                 <FormattedMessage
                     id="dokumentoversikt.info"
                     values={{
