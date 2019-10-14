f7c716f19b566830e2cc45c4edb38ea091f18d89
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-10-14 10:20:24 +0200

    Fix test

--- src/app/pages/ettersendelse/__tests__/ettersendelse.test.tsx
+++ src/app/pages/ettersendelse/__tests__/ettersendelse.test.tsx
@@ -258,7 +258,7 @@ describe('Ettersendelse page', () => {
             />
         ).shallow();
         const dropdown = wrapper.find({ className: 'ettersendelse__attachment-type-select' });
-        expect(dropdown.children().length).toBe(20);
+        expect(dropdown.children().length).toBe(19);
     });
 
     it('attachment type dropdown should only render relevant attachment types for foreldrepengesoknad with endring', () => {
