7d65fe7250ff30ee32fcd4a6d1ac3144b32298e2
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-10-16 09:49:18 +0200

    Update styling

--- src/common/storage/attachment/components/AttachmentOverview.tsx
+++ src/common/storage/attachment/components/AttachmentOverview.tsx
@@ -41,9 +41,10 @@ class AttachmentOverview extends React.Component<Props> {
                     />
                 );
             } else if (error && error.response !== undefined && error.response.status === 422) {
+                const intlId = error.response.message && error.respnse.message.contains('decrypt') ? 'vedlegg.passordbeskyttet' : 'vedlegg.virus';
                 errorMessages.push(
                     <FormattedMessage
-                        id={multipleErrors ? 'vedlegg.passordbeskyttet.flereFeil' : 'vedlegg.passordbeskyttet'}
+                        id={multipleErrors ? `${intlId}.flereFeil` : intlId}
                         values={{ filename: a.filename }}
                     />
                 );
