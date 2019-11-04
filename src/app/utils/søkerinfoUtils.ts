fd989084a10b60b6bd12fdcd20e65dd37b19a393
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-11-04 15:42:35 +0100

    Simplify function

--- src/app/utils/søkerinfoUtils.ts
+++ src/app/utils/søkerinfoUtils.ts
@@ -1,20 +1,12 @@
 import Arbeidsforhold from 'app/types/Arbeidsforhold';
 import moment, { Moment } from 'moment';
 
-export const getAktiveArbeidsforhold = (arbeidsforhold: Arbeidsforhold[], fraDato: Moment): Arbeidsforhold[] => {
-    return arbeidsforhold.reduce((aktiveArbeidsforhold: Arbeidsforhold[], a: Arbeidsforhold) => {
-        if (a.tom === undefined) {
-            aktiveArbeidsforhold.push(a);
-
-            return aktiveArbeidsforhold;
-        } else {
-            if (moment(a.tom).isSameOrAfter(fraDato, 'days')) {
-                aktiveArbeidsforhold.push(a);
-            }
-
-            return aktiveArbeidsforhold;
-        }
-    }, []);
+export const getAktiveArbeidsforhold = (arbeidsforhold: Arbeidsforhold[], fom: Moment): Arbeidsforhold[] => {
+    return arbeidsforhold.filter((a) => (
+        a.tom === undefined ||
+        a.tom === null ||
+        fom !== undefined && moment(fom).isSameOrBefore(a.tom, 'days')
+    ));
 };
 
 export const harAktivtArbeidsforhold = (arbeidsforhold: Arbeidsforhold[]): boolean => {
