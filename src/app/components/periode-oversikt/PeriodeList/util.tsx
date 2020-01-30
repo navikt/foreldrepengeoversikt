3b971a2a508f7181d60057596af2092034cb6eff
Andreas Dybdahl <andreas.dybdahl@nav.no>
2020-01-30 15:37:01 +0100

    Bruk korrekte ikoner for utsettelser

--- src/app/components/periode-oversikt/PeriodeList/util.tsx
+++ src/app/components/periode-oversikt/PeriodeList/util.tsx
@@ -1,4 +1,4 @@
-import Periode, { PeriodeType, Uttaksperiode } from 'app/types/uttaksplan/Periode';
+import Periode, { PeriodeType, Uttaksperiode, isUtsettelsesperiode } from 'app/types/uttaksplan/Periode';
 import {
     getStønadskontoFarge,
     erGradert,
@@ -11,6 +11,10 @@ import React from 'react';
 import UttakIkon from 'app/components/ikoner/UttakIkon';
 import { NavnPåForeldre } from 'common/components/oversikt-brukte-dager/OversiktBrukteDager';
 import { InjectedIntl } from 'react-intl';
+import { UtsettelsePeriodeType } from 'app/api/types/UttaksplanDto';
+import ArbeidIkon from 'app/components/ikoner/uttaksplanIkon/ikoner/ArbeidIkon';
+import SykdomIkon from 'app/components/ikoner/uttaksplanIkon/ikoner/SykdomIkon';
+import FerieIkon from 'app/components/ikoner/uttaksplanIkon/ikoner/FerieIkon';
 
 export const getIconFarge = (periode: Periode) => {
     switch (periode.type) {
@@ -24,6 +28,31 @@ export const getIconFarge = (periode: Periode) => {
 };
 
 export const getIkon = (periode: Periode) => {
+    if (isUtsettelsesperiode(periode)) {
+        switch (periode.årsak) {
+            case UtsettelsePeriodeType.Arbeid:
+                return (
+                    <IconBox color={getIconFarge(periode)}>
+                        <ArbeidIkon title="Arbeidsperiode ikon" />
+                    </IconBox>
+                );
+            case UtsettelsePeriodeType.BarnInnlagt:
+            case UtsettelsePeriodeType.SykdomSkade:
+            case UtsettelsePeriodeType.SøkerInnlagt:
+                return (
+                    <IconBox color={getIconFarge(periode)}>
+                        <SykdomIkon title="Sykdomsperiode ikon" />
+                    </IconBox>
+                );
+            case UtsettelsePeriodeType.Ferie:
+                return (
+                    <IconBox color={getIconFarge(periode)}>
+                        <FerieIkon title="Uttaksperiode ikon" />
+                    </IconBox>
+                );
+        }
+    }
+
     return (
         <IconBox color={getIconFarge(periode)} stripes={erGradert(periode)}>
             <UttakIkon title="uttak ikon" />
