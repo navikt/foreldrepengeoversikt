e91cd3fd37314419e71cf60f0ce8fc8549dd5d1a
Andreas Dybdahl <andreas.dybdahl@nav.no>
2020-04-23 09:38:28 +0200

    Oppdatert feature i q

--- src/app/Environment.js
+++ src/app/Environment.js
@@ -3,10 +3,11 @@ const Environment = () => {
         REST_API_URL: window.appSettings.REST_API_URL,
         LOGIN_URL: window.appSettings.LOGIN_URL,
         UTTAK_API_URL: window.appSettings.UTTAK_API_URL,
-        ["FEATURE_HISTORIKK"]: window.appSettings["FEATURE_HISTORIKK"],
-        ["FEATURE_MINI_DIALOG"]: window.appSettings["FEATURE_MINI_DIALOG"],
-        ["FEATURE_DIN_PLAN"]: window.appSettings["FEATURE_DIN_PLAN"],
-        ["FEATURE_KONTOOVERSIKT"]: window.appSettings["FEATURE_KONTOOVERSIKT"]
+        ['FEATURE_HISTORIKK']: window.appSettings['FEATURE_HISTORIKK'],
+        ['FEATURE_MINI_DIALOG']: window.appSettings['FEATURE_MINI_DIALOG'],
+        ['FEATURE_DIN_PLAN']: window.appSettings['FEATURE_DIN_PLAN'],
+        ['FEATURE_KONTOOVERSIKT']: window.appSettings['FEATURE_KONTOOVERSIKT'],
+        ['FEATURE_SAKSOVERSIKT']: window.appSettings['FEATURE_SAKSOVERSIKT']
     };
 };
 
