e5cd013e1f8ebb0bc90728053d38c9a11f2d5c20
Andreas Dybdahl <andreas.dybdahl@nav.no>
2020-06-22 12:41:26 +0200

    More upgrades

--- src/app/Foreldrepengeoversikt.tsx
+++ src/app/Foreldrepengeoversikt.tsx
@@ -33,7 +33,7 @@ interface Props {
 }
 
 class Foreldrepengeoversikt extends React.Component<Props> {
-    componentWillMount(): void {
+    componentDidMount(): void {
         this.fetchData();
     }
 
@@ -97,7 +97,7 @@ const mapStateToProps = (state: AppState) => {
                 oppslag.status === FetchStatus.UNFETCHED ||
                 oppslag.status === FetchStatus.IN_PROGRESS ||
                 getErrorCode(oppslag) === 401
-        )
+        ),
     };
 };
 
@@ -113,10 +113,7 @@ const mapDispatchToProps = (dispatch: (action: ApiAction) => void) => ({
     },
     requestHistorikk: () => {
         dispatch({ type: ApiActionTypes.GET_HISTORIKK_REQUEST });
-    }
+    },
 });
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Foreldrepengeoversikt);
+export default connect(mapStateToProps, mapDispatchToProps)(Foreldrepengeoversikt);
