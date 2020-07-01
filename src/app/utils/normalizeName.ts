b6b10212d6ada2e73acf247d5d569dd0e2235773
Andreas Dybdahl <andreas.dybdahl@nav.no>
2020-07-01 14:14:40 +0200

    Another attempt

--- src/app/utils/normalizeName.ts
+++ src/app/utils/normalizeName.ts
@@ -1,8 +1,100 @@
-// prettier-ignore
-const preposisjonerNor = ['OM', 'OVENFOR', 'ETTER', 'MOT', 'BLANT', 'RUNDT', 'SOM', 'PÅ', 'FØR', 'BAK', 'NEDENFOR', 'UNDER', 'MELLOM', 'UTOVER', 'MEN', 'AV', 'NED', 'UNNTATT', 'FOR', 'FRA', 'I', 'INNSIDEN', 'INN', 'NESTE', 'AV', 'PÅ', 'MOTSATT', 'UT', 'UTENFOR', 'OVER', 'PER', 'RUNDE', 'SIDEN', 'ENN', 'GJENNOM', 'TIL', 'TIL', 'MOT', 'UNDER', 'TIL', 'OPP', 'VIA', 'MED', 'INNEN', 'UTEN', 'DETTE'];
+const preposisjonerNor = [
+    'OM',
+    'OVENFOR',
+    'ETTER',
+    'MOT',
+    'BLANT',
+    'RUNDT',
+    'SOM',
+    'PÅ',
+    'FØR',
+    'BAK',
+    'NEDENFOR',
+    'UNDER',
+    'MELLOM',
+    'UTOVER',
+    'MEN',
+    'AV',
+    'NED',
+    'UNNTATT',
+    'FOR',
+    'FRA',
+    'I',
+    'INNSIDEN',
+    'INN',
+    'NESTE',
+    'AV',
+    'PÅ',
+    'MOTSATT',
+    'UT',
+    'UTENFOR',
+    'OVER',
+    'PER',
+    'RUNDE',
+    'SIDEN',
+    'ENN',
+    'GJENNOM',
+    'TIL',
+    'TIL',
+    'MOT',
+    'UNDER',
+    'TIL',
+    'OPP',
+    'VIA',
+    'MED',
+    'INNEN',
+    'UTEN',
+    'DETTE',
+];
 
-// prettier-ignore
-const preposisjonerEng = ['ABOUT', 'ABOVE', 'AFTER', 'AGAINST', 'AMONG', 'AROUND', 'AS', 'AT', 'BEFORE', 'BEHIND', 'BELOW', 'BENEATH', 'BETWEEN', 'BEYOND', 'BUT', 'BY', 'DOWN', 'EXCEPT', 'FOR', 'FROM', 'IN', 'INSIDE', 'INTO', 'NEXT', 'OF', 'ON', 'OPPOSITE', 'OUT', 'OUTSIDE', 'OVER', 'PER', 'ROUND', 'SINCE', 'THAN', 'THROUGH', 'TILL', 'TO', 'TOWARD', 'UNDER', 'UNTIL', 'UP', 'VIA', 'WITH', 'WITHIN', 'WITHOUT', 'THIS'];
+const preposisjonerEng = [
+    'ABOUT',
+    'ABOVE',
+    'AFTER',
+    'AGAINST',
+    'AMONG',
+    'AROUND',
+    'AS',
+    'AT',
+    'BEFORE',
+    'BEHIND',
+    'BELOW',
+    'BENEATH',
+    'BETWEEN',
+    'BEYOND',
+    'BUT',
+    'BY',
+    'DOWN',
+    'EXCEPT',
+    'FOR',
+    'FROM',
+    'IN',
+    'INSIDE',
+    'INTO',
+    'NEXT',
+    'OF',
+    'ON',
+    'OPPOSITE',
+    'OUT',
+    'OUTSIDE',
+    'OVER',
+    'PER',
+    'ROUND',
+    'SINCE',
+    'THAN',
+    'THROUGH',
+    'TILL',
+    'TO',
+    'TOWARD',
+    'UNDER',
+    'UNTIL',
+    'UP',
+    'VIA',
+    'WITH',
+    'WITHIN',
+    'WITHOUT',
+    'THIS',
+];
 
 const preposisjoner = [...preposisjonerNor, ...preposisjonerEng];
 const forkortelser = ['AS', 'ASA'];
@@ -28,17 +120,21 @@ const normalizeName = (navn: string) => {
 
     return navn
         .split(COMMA)
-        .map((chunkOfName) =>
-            chunkOfName
-                .split(SPACE)
-                .map((word, wordIndex) =>
-                    word
-                        .split(HYPHEN)
-                        .map(normalizeWord(wordIndex === 0))
-                        .join(HYPHEN)
-                )
-                .join(SPACE)
-        )
+        .map((chunkOfName) => {
+            if (chunkOfName) {
+                return chunkOfName
+                    .split(SPACE)
+                    .map((word, wordIndex) =>
+                        word
+                            .split(HYPHEN)
+                            .map(normalizeWord(wordIndex === 0))
+                            .join(HYPHEN)
+                    )
+                    .join(SPACE);
+            }
+
+            return '';
+        })
         .join(COMMA);
 };
 
