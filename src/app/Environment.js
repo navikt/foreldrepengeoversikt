a5b5dbda4a69216c2b59680fb29516cf056462b8
Andreas Dybdahl <andreas.dybdahl@nav.no>
2020-08-17 16:16:13 +0200

    Lagt til knapp for å klage på vedtakk

--- src/app/Environment.js
+++ src/app/Environment.js
@@ -3,6 +3,7 @@ const Environment = () => {
         REST_API_URL: window.appSettings.REST_API_URL,
         LOGIN_URL: window.appSettings.LOGIN_URL,
         UTTAK_API_URL: window.appSettings.UTTAK_API_URL,
+        KLAGE_URL: window.appSettings.KLAGE_URL,
     };
 };
 
--- src/build/scripts/envSettings.js
+++ src/build/scripts/envSettings.js
@@ -8,7 +8,8 @@ function createEnvSettingsFile(settingsFile) {
                 REST_API_URL: '${process.env.FORELDREPENGESOKNAD_API_URL}',
                 LOGIN_URL: '${process.env.LOGINSERVICE_URL}',
                 UTTAK_API_URL: '${process.env.FP_UTTAK_SERVICE_URL}',
-                APPRES_CMS_URL: '${process.env.APPRES_CMS_URL}'
+                APPRES_CMS_URL: '${process.env.APPRES_CMS_URL}',
+                KLAGE_URL: '${process.env.KLAGE_URL}'
             };`
         );
     });
