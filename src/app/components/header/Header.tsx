28be5dd7d7105cb7695baabb66155e91ff48ae75
Andreas Dybdahl <andreas.dybdahl@nav.no>
2023-01-16 18:23:28 +0100

    Link i header

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,20 +1,30 @@
-import { Heading } from '@navikt/ds-react';
+import { BodyShort, Heading } from '@navikt/ds-react';
 import { bemUtils } from '@navikt/fp-common';
 import TåteflaskeBaby from 'assets/TåteflaskeBaby';
 import React from 'react';
+import PreviousLink from '../previous-link/PreviousLink';
 
 import './header.css';
 
-const Header: React.FunctionComponent = () => {
+interface Props {
+    route: string;
+    externalURL?: boolean;
+    linkLabel: string;
+}
+
+const Header: React.FunctionComponent<Props> = ({ route, externalURL = false, linkLabel }) => {
     const bem = bemUtils('header');
 
     return (
         <div className={bem.block}>
             <div className={bem.element('wrapper')}>
-                <TåteflaskeBaby />
-                <div className={bem.element('title-container')}>
-                    <Heading size="xlarge">Foreldrepengene mine</Heading>
-                    <p>Stuff</p>
+                <PreviousLink route={route} externalURL={externalURL} linkLabel={linkLabel} />
+                <div className={bem.element('content')}>
+                    <TåteflaskeBaby />
+                    <div className={bem.element('title-container')}>
+                        <Heading size="xlarge">Foreldrepengene mine</Heading>
+                        <BodyShort>PENGESTØTTE</BodyShort>
+                    </div>
                 </div>
             </div>
         </div>
--- src/app/routes/ForeldrepengeoversiktRoutes.tsx
+++ src/app/routes/ForeldrepengeoversiktRoutes.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import OversiktRoutes from './routes';
-import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import Saksoversikt from 'app/pages/Saksoversikt';
 import { bemUtils } from '@navikt/fp-common';
 import { SøkerinfoDTO } from 'app/types/SøkerinfoDTO';
@@ -10,19 +10,31 @@ import SeSøknadPage from 'app/pages/se-søknad-page/SeSøknadPage';
 import { default as SakComponent } from 'app/pages/Sak';
 import DinPlanPage from 'app/pages/din-plan-page/DinPlanPage';
 import DokumenterPage from 'app/pages/dokumenter-page/DokumenterPage';
-
-import './routes-wrapper.css';
 import Forside from 'app/pages/forside/Forside';
 import Header from 'app/components/header/Header';
+
+import './routes-wrapper.css';
 
 interface Props {
     søkerinfo: SøkerinfoDTO;
     foreldrepengerSaker: Sak[];
 }
 
+const getHeaderRouteInfo = (path: string) => {
+    if (path.length > 1) {
+        return { route: OversiktRoutes.HOVEDSIDE, label: 'Mine foreldrepenger', isExternalURL: false };
+    }
+
+    return { route: 'https://www.nav.no/no/ditt-nav', label: 'Min side', isExternalURL: true };
+};
+
 const ForeldrepengeoversiktRoutes: React.FunctionComponent<Props> = ({ søkerinfo, foreldrepengerSaker }) => {
     const bem = bemUtils('routesWrapper');
     const navigate = useNavigate();
+    const location = useLocation();
+    const path = location.pathname;
+    console.log(path);
+    console.log(location);
 
     useEffect(() => {
         if (foreldrepengerSaker.length === 1) {
@@ -34,9 +46,15 @@ const ForeldrepengeoversiktRoutes: React.FunctionComponent<Props> = ({ søkerinf
         }
     }, [foreldrepengerSaker]);
 
+    const headerRouteInfo = getHeaderRouteInfo(path);
+
     return (
         <>
-            <Header />
+            <Header
+                route={headerRouteInfo.route}
+                linkLabel={headerRouteInfo.label}
+                externalURL={headerRouteInfo.isExternalURL}
+            />
             <div className={bem.block}>
                 <Routes>
                     <Route path="/" element={<Forside saker={foreldrepengerSaker} />} />
